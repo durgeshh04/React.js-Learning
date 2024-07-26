350fa7823faa5aa8215ff1ed9dc2da777af6502c
Durgesh Chaudhari <133797261+durgeshh04@users.noreply.github.com>
2024-07-26 23:58:15 +0530

    Update InputRef.jsx

--- src/components/InputRef.jsx
+++ src/components/InputRef.jsx
@@ -1,13 +1,13 @@
 // Taking input from ForwadingRefs.jsx: Practicing forwardRef
 
-import React, {forwardRef} from 'react'
+import React, { forwardRef } from "react";
 
 const InputRef = forwardRef((props, ref) => {
   return (
     <div>
-      <input ref={ref} type="text" />
+      <input style={{ outline: "none" }} ref={ref} type="text" />
     </div>
-  )
-})
+  );
+});
 
-export default InputRef
+export default InputRef;
